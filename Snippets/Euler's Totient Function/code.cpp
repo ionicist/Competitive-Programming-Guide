b0e221ceffdadaa10e93b0ea71196c6a9d7631ec
Lakshay Kumar <cmd.lakshay@gmail.com>
2026-01-29 17:50:03 +0530

    Create code.cpp

---
+++ Snippets/Euler's Totient Function/code.cpp
@@ -0,0 +1,15 @@
+long long eulers_totient_function(long long n) {
+    long long res = n;
+    for (long long i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0) {
+                n /= i;
+            }
+            res -= res / i;
+        }
+    }
+    if (n != 1) {
+        res -= res / n;
+    }
+    return res;
+}
